d9c4f03e40a6bad760596c106210a0bb758d5570
Piyush0685 <piyushughade17@gmail.com>
2026-03-28 16:54:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 
-// 🔥 THIS LINE FIXES YOUR ISSUE
-app.use(express.static("public"));
+app.use(express.static("public")); // 🔥 VERY IMPORTANT
 
 app.use(express.json());
 
